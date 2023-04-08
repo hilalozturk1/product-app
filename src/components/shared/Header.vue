6c0b237ba245ae0347f3f627183c535380f7008a
hilalozturk1 <hlloztrk68@gmail.com>
2023-04-08 15:36:34 +0300

    navigations

--- src/components/shared/Header.vue
+++ src/components/shared/Header.vue
@@ -1,14 +1,30 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #563F7A">
-    <a class="navbar-brand" href="#">Product List</a>
+  <nav
+    class="navbar navbar-expand-lg navbar-dark"
+    style="background-color: #563F7A"
+  >
+    <a class="navbar-brand" href="#"></a>
+    <router-link to="/" class="navbar-brand" tag="a">
+      Product List
+    </router-link>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav mr-auto">
-        <li class="nav-item">
-          <a class="nav-link" href="#">Product Operations</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Prodcut Quit</a>
-        </li>
+        <router-link
+          to="/product-operations"
+          tag="li"
+          class="nav-item"
+          active-class="active"
+        >
+          <a class="nav-link">Product Operations</a>
+        </router-link>
+        <router-link
+          to="/product-quit"
+          tag="li"
+          class="nav-item"
+          active-class="active"
+        >
+          <a class="nav-link">Prodcut Quit</a>
+        </router-link>
       </ul>
     </div>
   </nav>
