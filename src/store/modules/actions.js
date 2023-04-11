f21db3f1825ba801d57c58bb67b1aef2ad1cfe01
hilalozturk1 <hlloztrk68@gmail.com>
2023-04-11 14:30:03 +0300

    set trade result

--- src/store/modules/actions.js
+++ src/store/modules/actions.js
@@ -1,3 +1,20 @@
-export const setTradeResult = ({ state }) => {};
+import Vue from "vue";
+
+export const setTradeResult = ({ state, commit }, tradeResult) => {
+  commit("updateTradeResult", tradeResult); 
+
+  let tradeData = {
+    purchase: state.purchase,
+    sale: state.sale
+  };
+  Vue.http
+    .put(
+      "https://product-operations-74152-default-rtdb.firebaseio.com/trade-result.json",
+      tradeData
+    )
+    .then(response => {
+      console.log("response", response);
+    }); 
+};
 
 export const getTradeResult = ({ commit }) => {};
